fdca4bc5403dbe95e17aa956889a85b720687ddb
Pedro Fraga <antoniopedrofraga@gmail.com>
2015-10-16 17:12:39 +0100

    Exercicios de php feitos na aula

---
+++ Fourth Class/sum2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title>Resultado</title>
+			<meta charset = "utf-8">
+		</head>
+
+		<body>
+		<h1>Result</h1><br>
+		<?php
+		echo $_GET["num1"] . '+' . $_GET["num2"] . ' = ' . ($_GET["num2"] + $_GET["num1"]);
+		?>
+		<br>
+		<br>
+		<a href="http://gnomo.fe.up.pt/~up201303095/form2.html">Voltar</a>
+		</body>
+	</html>
+
---
+++ Fourth Class/teste.php
@@ -0,0 +1,5 @@
+<?php
+$homepage = file_get_contents('http://moodle.up.pt/moodlewatcher/index2.php');
+echo $homepage;
+echo "a";
+?>
